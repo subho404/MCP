b21f4e2b56bf1a7ba2f88eaa8af4c579f1ca7c12
subho404 <subhoanjan3@gmail.com>
2025-09-06 12:15:03 +0530

    first commit

---
+++ client/index.js
@@ -0,0 +1,11 @@
+const readline=require('readline/promises');
+
+const chatHistory=[];
+
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+})
+async function chatloop() {
+    
+}
